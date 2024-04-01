e825f5b9ad1b1c3f64584286d8d2854850230cc8
Suree33 <d@sur33.com>
2024-04-01 09:17:55 +0900

    Pretty RSS Feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,20 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import config from '../config.json';
+import sanitizeHtml from 'sanitize-html';
 
 export async function GET() {
+  const postImportResult = import.meta.glob('./**/*.md', { eager: true });
+  const posts = Object.values(postImportResult);
   return rss({
     title: config.siteName,
     description: config.description,
     site: config.url,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    stylesheet: '/rss/styles.xsl',
+    items: posts.map((post) => ({
+      link: post.url,
+      content: sanitizeHtml(post.compiledContent()),
+      ...post.frontmatter,
+    })),
     customData: `<language>ja-jp</language>`,
   });
 }
