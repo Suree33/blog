cc007ffcc01480667e15cd00a97e826525b99773
Suree33 <d@sur33.com>
2024-04-01 09:31:39 +0900

    Vercel Speed Insights & Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +11,8 @@ export default defineConfig({
     }),
     icon(),
   ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
