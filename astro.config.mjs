012d002b9932c7efca8f8aed4c84f3ff493ec994
Suree33 <d@sur33.com>
2024-04-04 19:39:36 +0900

    サイトマップを追加しました

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true },
+    webAnalytics: {
+      enabled: true,
+    },
   }),
+  site: 'https://sur33.com/',
   integrations: [
     tailwind({
       nesting: true,
     }),
+    sitemap(),
     icon(),
   ],
 });
