33e49af694aa5f744b74ec4c4d7be4987c346873
Suree33 <d@sur33.com>
2024-04-05 02:58:17 +0900

    コードブロックをダークモードとライトモードで別の色に

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,10 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      theme: 'dracula',
+      themes: {
+        dark: 'github-dark',
+        light: 'github-light',
+      },
     },
   },
   site: 'https://sur33.com/',
