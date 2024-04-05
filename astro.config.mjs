2f6e6e7a6f1adbe076b8ce4e289f0de13dd4523b
Suree33 <d@sur33.com>
2024-04-05 19:18:10 +0900

    Fix Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import remarkCodeTitles from 'remark-flexible-code-titles';
 import remarkLinkCard from 'remark-link-card';
-
-import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
@@ -36,5 +35,10 @@ export default defineConfig({
     sitemap(),
     icon(),
     mdx(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
   ],
 });
