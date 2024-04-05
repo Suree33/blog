06e2ab2d4c65dc62d75e784cc07ec0844d700c7f
Suree33 <d@sur33.com>
2024-04-05 19:07:32 +0900

    Fix Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import remarkCodeTitles from 'remark-flexible-code-titles';
 import remarkLinkCard from 'remark-link-card';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
