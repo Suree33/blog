89871703417d2bbe0ba0f6d24b67d2ba9bfe424a
Suree33 <d@sur33.com>
2024-04-05 18:57:40 +0900

    Vercel AnalyticsからGoogle Analyticsに乗り換え

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import remarkCodeTitles from 'remark-flexible-code-titles';
 import remarkLinkCard from 'remark-link-card';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   markdown: {
     remarkPlugins: [
       remarkCodeTitles,
@@ -41,5 +35,10 @@ export default defineConfig({
     sitemap(),
     icon(),
     mdx(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
   ],
 });
