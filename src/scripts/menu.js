dc79b9fd30706a127809b0a6ebf6c492a52c4645
Suree33 <d@sur33.com>
2024-04-07 04:38:25 +0900

    ヘッダーのスクロールに応じた表示/非表示を修正

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -45,10 +45,18 @@ export const toggleHamburgerMenu = (
       }
       if (hamburger.classList.contains('active')) {
         menu.forEach((item) => item.classList.add('active'));
+        disableScroll();
       } else {
         menu.forEach((item) => item.classList.remove('active'));
+        enableScroll();
       }
     });
+  }
+  function disableScroll() {
+    document.body.style.overflow = 'hidden';
+  }
+  function enableScroll() {
+    document.body.style.overflow = 'visible';
   }
 };
 
@@ -61,6 +69,7 @@ export const toggleHamburgerMenu = (
  * @param {Object} [options={}] - オプション設定
  * @param {number} [options.upThreshold=1500] - 上スクロールの速度しきい値（ピクセル/秒）
  * @param {number} [options.downThreshold=1000] - 下スクロールの速度しきい値（ピクセル/秒）
+ * @param {string} hamburgerSelector - ハンバーガーメニューの要素を指定するCSSセレクタ
  * @description スクロールイベントを監視し、スクロール速度に応じてヘッダーを隠したり表示したりする。
  * 上にスクロールした場合、スクロール速度が上スクロールのしきい値を超えるとヘッダーを表示する。
  * 下にスクロールした場合、スクロール速度が下スクロールのしきい値を超えるとヘッダーを隠す。
@@ -68,34 +77,39 @@ export const toggleHamburgerMenu = (
 export const toggleHeaderOnScroll = (
   headerSelector,
   options = { upThreshold: 1500, downThreshold: 1000 },
+  hamburgerSelector = null,
 ) => {
   const { upThreshold = 50, downThreshold = 50 } = options;
   let prevScrollpos = window.pageYOffset;
   let prevTime = Date.now();
   const header = document.querySelector(headerSelector);
+  const hamburger = document.querySelector(hamburgerSelector);
 
   window.addEventListener('scroll', () => {
-    const currentScrollPos = window.pageYOffset;
-    const currentTime = Date.now();
-    const elapsedTime = currentTime - prevTime;
+    if (!(hamburger && hamburger.classList.contains('active'))) {
+      const currentScrollPos = window.pageYOffset;
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - prevTime;
 
-    // スクロール速度を計算（ピクセル/秒）
-    const scrollSpeed =
-      Math.abs(currentScrollPos - prevScrollpos) / (elapsedTime / 1000);
+      // スクロール速度を計算（ピクセル/秒）
+      const scrollSpeed =
+        Math.abs(currentScrollPos - prevScrollpos) / (elapsedTime / 1000);
 
-    if (prevScrollpos > currentScrollPos) {
-      // 上にスクロールした場合
-      if (scrollSpeed > upThreshold) {
-        header.style.top = '0';
+      if (prevScrollpos > currentScrollPos) {
+        // 上にスクロールした場合
+        if (scrollSpeed > upThreshold) {
+          header.style.top = '0';
+        }
+      } else {
+        // 下にスクロールした場合
+        if (scrollSpeed > downThreshold) {
+          header.style.top = `-${header.offsetHeight}px`;
+        }
       }
+
+      prevScrollpos = currentScrollPos;
+      prevTime = currentTime;
     } else {
-      // 下にスクロールした場合
-      if (scrollSpeed > downThreshold) {
-        header.style.top = `-${header.offsetHeight}px`;
-      }
     }
-
-    prevScrollpos = currentScrollPos;
-    prevTime = currentTime;
   });
 };
