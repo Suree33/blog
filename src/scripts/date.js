a6b93580d9c9426b367f74e4905090425243a830
Suree33 <d@sur33.com>
2024-04-22 19:17:47 +0900

    日付の表示を変更

--- src/scripts/date.js
+++ src/scripts/date.js
@@ -1,7 +1,19 @@
-export function formatDate(date) {
-  return new Date(date).toLocaleDateString('ja-JP', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+export function formatDate(dateString) {
+  if (!dateString) {
+    return false;
+  }
+  const date = new Date(dateString);
+  if (isValidDate(date)) {
+    return date.toLocaleDateString('ja-JP', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  } else {
+    return false;
+  }
+}
+
+function isValidDate(date) {
+  return date instanceof Date && !isNaN(date.getTime());
 }
