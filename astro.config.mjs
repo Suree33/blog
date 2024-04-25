3b440ef32e50804a4b647b8aa7fd318e2ec95795
Suree33 <d@sur33.com>
2024-04-25 19:19:12 +0900

    リダイレクトを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,6 @@ export default defineConfig({
   site: 'https://sur33.com/',
   redirects: {
     '/posts': '/',
-    '/blog': '/',
-    '/articles': '/',
-    '/blog/[...slug]': '/posts/[...slug]',
-    '/articles/[...slug]': '/posts/[...slug]',
   },
   markdown: {
     remarkPlugins: [
