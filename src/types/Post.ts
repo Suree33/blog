08e4152301c1d940241e8cfc561eeddbc3723718
Suree33 <d@sur33.com>
2025-01-01 00:28:03 +0900

    refactor: separate Frontmatter interface from Post type definition

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,19 +1,21 @@
-export interface Post {
-  frontmatter: {
-    title: string;
-    pubDate: string;
-    updatedDate?: string;
-    description: string;
-    tags: string[];
-    draft?: boolean;
-    ogimage?: {
-      src: string;
-      alt: string;
-    };
-    image?: {
-      src: string;
-      alt: string;
-    };
+export interface Frontmatter {
+  title: string;
+  pubDate: string;
+  updatedDate?: string;
+  description: string;
+  tags: string[];
+  draft?: boolean;
+  ogimage?: {
+    src: string;
+    alt: string;
   };
+  image?: {
+    src: string;
+    alt: string;
+  };
+}
+
+export interface Post {
+  frontmatter: Frontmatter;
   url: string;
 }
