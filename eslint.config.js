28d6c100121980132fc73f2c8e0fe58080df28f0
Suree33 <d@sur33.com>
2025-01-01 01:36:06 +0900

    install typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import eslintPluginAstro from 'eslint-plugin-astro';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
   js.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   eslintConfigPrettier,
   ...eslintPluginAstro.configs['flat/recommended'],
   ...eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
