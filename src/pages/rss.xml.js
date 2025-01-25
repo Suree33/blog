80d3e4167675147f0ae6a3c4efca7bd71e006992
Suree33 <d@sur33.com>
2025-01-26 04:30:42 +0900

    feat: Postインターフェースに新しいフィールドを追加し、RSSフィードとタグページの処理を改善

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,18 +3,24 @@ import config from '../config.json';
 import sanitizeHtml from 'sanitize-html';
 
 export async function GET() {
-  const postImportResult = import.meta.glob('./**/*.md', { eager: true });
-  const posts = Object.values(postImportResult);
+  const posts = await Promise.all(
+    Object.values(import.meta.glob('./**/*.md', { eager: true }))
+  );
+
   return rss({
     title: config.siteName,
     description: config.description,
     site: config.url,
     stylesheet: '/rss/styles.xsl',
-    items: posts.map((post) => ({
-      link: post.url,
-      content: sanitizeHtml(post.compiledContent()),
-      ...post.frontmatter,
-    })),
+    items: posts
+      .filter(post => post.frontmatter.title && post.frontmatter.pubDate)
+      .map((post) => ({
+        title: post.frontmatter.title,
+        description: post.frontmatter.description || '',
+        pubDate: new Date(post.frontmatter.pubDate),
+        link: post.url,
+        content: sanitizeHtml(post.compiledContent()),
+      })),
     customData: `<language>ja-jp</language>`,
   });
 }
