1538bcfcde39acdf139ebf1c8b4e4d22c96c35d8
Suree33 <d@sur33.com>
2025-03-30 19:42:48 +0900

    環境変数にUnsplash APIキーの型定義を追加

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 import type {} from '../.astro/types.d.ts';
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly UNSPLASH_ACCESS_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
